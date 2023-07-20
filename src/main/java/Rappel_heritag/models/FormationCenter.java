4ba25e16e79f9f5238be105f377c726e85181ee9
Stagiaire <bilal-b@live.be>
2023-07-20 16:15:33 +0200

    Update fin-journé

--- src/main/java/Rappel_heritag/models/FormationCenter.java
+++ src/main/java/Rappel_heritag/models/FormationCenter.java
@@ -1,67 +1,134 @@
 package Rappel_heritag.models;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
 
 public class FormationCenter {
 
-    // Const
-    public final LocalTime MIN_OPEN_TIME = LocalTime.of(5,0);
-    public final LocalTime MAX_CLOSE_TIM = LocalTime.of(21,0);
+    // Constantes (Constants)
+    public final LocalTime MIN_OPEN_TIME = LocalTime.of(5, 0);
+    public final LocalTime MAX_CLOSE_TIME = LocalTime.of(21, 0);
 
-    // Champ (Fields)
+    // Champs (Fields)
     private String name;
-    private String address; //TODO Remplace by class Adress
+    private String address; // TODO Remplace by classe Address
     private LocalTime openTime;
     private LocalTime closeTime;
+    private HashMap<String, TrainingPath> trainings;
 
 
-
-    // Constructeur
-
-
+    // Constructeurs (Constructors)
     public FormationCenter(String name, String address, LocalTime openTime, LocalTime closeTime) {
+        if (name == null || name.trim().isEmpty()  /* || name.trim().length() == 0 */) {
+            throw new RuntimeException("Le nom du centre de formateur ne peut pas etre vide !");
+        }
+        if (openTime.isBefore(MIN_OPEN_TIME) || closeTime.isAfter(MAX_CLOSE_TIME)) {
+            throw new RuntimeException("L'horraire n'est pas valide !");
+        }
+
         this.name = name;
         this.address = address;
         this.openTime = openTime;
         this.closeTime = closeTime;
+        trainings = new HashMap<>();
     }
 
+    // Accesseurs (Getters)
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        if (name == null || name.trim().isEmpty()) /* || name.trim().length() == 0 || name.trim().isEqual("")*/ {
-            throw new RuntimeException("Le nom du centre de formateur ne peut pas etre vide !");
-        }
-        this.name = name;
+        return name.toUpperCase();
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public LocalTime getOpenTime() {
         return openTime;
-    }
-
-    public void setOpenTime(LocalTime openTime) {
-        if (openTime != null && openTime.isBefore(MIN_OPEN_TIME) ){
-            this.openTime = MIN_OPEN_TIME;
-            return;
-        }
-        this.openTime = openTime;
     }
 
     public LocalTime getCloseTime() {
         return closeTime;
     }
 
-    public void setCloseTime(LocalTime closeTime) {
-        this.closeTime = (closeTime != null && closeTime.isAfter(MAX_CLOSE_TIM)) ? MAX_CLOSE_TIM : closeTime;
+    public TrainingPath getTraining(String trainingCode) {
+        if (trainingCode == null || trainingCode.isBlank()) {
+            throw new IllegalArgumentException("trainingCode cannot be \"null\" or empty");
+        }
+
+        return trainings.get(trainingCode);
     }
+
+    public List<TrainingPath> getTrainings() {
+        return List.copyOf(trainings.values());
+    }
+
+    // Mutateurs (Setters)
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()  /* || name.trim().length() == 0 */) {
+            throw new RuntimeException("Le nom du centre de formateur ne peut pas etre vide !");
+        }
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setOpenTime(LocalTime openTime) {
+        if (openTime != null && openTime.isBefore(MIN_OPEN_TIME)) {
+            this.openTime = MIN_OPEN_TIME;
+            return;
+        }
+
+        this.openTime = openTime;
+    }
+
+    public void setCloseTime(LocalTime closeTime) {
+        // Si closeTime n'est pas null et après MAX_CLOSE_TIME alors la valeur est MAX_CLOSE_TIME sinon c'est closeTime
+        this.closeTime = (closeTime != null && closeTime.isAfter(MAX_CLOSE_TIME)) ? MAX_CLOSE_TIME : closeTime;
+    }
+
+
+    // Méthodes (Method)
+    public Boolean addTraining(String code, String name, int nbStudent, LocalDate startDate, LocalDate endDate) {
+        if (nbStudent < 1 || nbStudent > 20) {
+            return false;
+        }
+
+        if(trainings.containsKey(code)) {
+            return false;
+        }
+
+        TrainingPath trainingPath = new TrainingPath(name, code, (byte) nbStudent, startDate, endDate);
+        trainings.put(code, trainingPath);  // trainings.putIfAbsent(code, trainingPath);
+
+        return true;
+    }
+
+    public Boolean addTraining(String code, String name, int nbStudent, LocalDate endDate) {
+        return addTraining(code, name, nbStudent, LocalDate.now(), endDate);
+    }
+
+    public Boolean remvoveTrainings(String code) {
+        TrainingPath deleteValue = trainings.remove(code);
+        return deleteValue != null;
+    }
+
+    public Boolean updateTraining(String code,String name, int nbStudent, LocalDate startDate, LocalDate endDate){
+        if (nbStudent < 1 || nbStudent > 20) {
+            throw new RuntimeException("Le nombre de stagiere doit etre entre 1 et 20 ");
+        }
+        if (!trainings.containsKey(code)) {
+            return false;
+        }
+        TrainingPath target = trainings.get(code);
+        target.setName(name);
+        target.setNbStudent((byte) nbStudent);
+        target.setStartDate(startDate);
+        target.setEndDate(endDate);
+        return true;
+    }
+
 }
--- src/main/java/Rappel_heritag/models/TrainingPath.java
+++ src/main/java/Rappel_heritag/models/TrainingPath.java
@@ -4,40 +4,44 @@ import java.time.LocalDate;
 
 public class TrainingPath {
 
-    // Champs - Fields (Attributes)
-
+    // Champs -> Attributes
     private String name;
+    private String code;
     private LocalDate startDate;
     private LocalDate endDate;
-    private byte  nbStudent;
+    private byte nbStudent;
 
-    // Consructeur
-
-
-    public TrainingPath(String name, byte nbStudent) {
-        if (nbStudent <= 0){
-            throw new RuntimeException("Le nombre de stagiere dois etre positif");
+    // Constructeur
+    public TrainingPath(String name, String code, byte nbStudent) {
+        if(nbStudent <= 0) {
+            throw new RuntimeException("Le nombre de stagiaire doit être positif");
         }
+
         this.name = name;
         this.nbStudent = nbStudent;
+        this.code = code;
     }
 
-    public TrainingPath(String name, LocalDate startDate, LocalDate endDate, byte nbStudent) {
-        this(name,nbStudent);
-        if (startDate.isAfter(endDate)){
-            throw new RuntimeException("Date invalide");
+    public TrainingPath(String name, String code, byte nbStudent, LocalDate startDate, LocalDate endDate) {
+        // L'appel du "this" (ou "super") doit toujours être la premier ligne executé (╯°□°）╯︵ ┻━┻
+        this(name, code, nbStudent);
+
+        if(startDate.isAfter(endDate)) {
+            throw  new RuntimeException("Date invalide !");
         }
 
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-//    Getter
-
+    // Getters
     public String getName() {
         return name;
     }
 
+    public String getCode() {
+        return code;
+    }
     public LocalDate getStartDate() {
         return startDate;
     }
@@ -50,28 +54,32 @@ public class TrainingPath {
         return nbStudent;
     }
 
+    // Setters
     public void setName(String name) {
         this.name = name;
     }
 
     public void setStartDate(LocalDate startDate) {
-        if (startDate != null && endDate != null && !startDate.isBefore(endDate)){
+        if(startDate != null && this.endDate != null && startDate.isAfter(endDate)) {
             throw new RuntimeException("La date de debut doit etre avant la date de fin");
         }
+
         this.startDate = startDate;
     }
 
     public void setEndDate(LocalDate endDate) {
-        if (startDate != null && endDate != null && !endDate.isAfter(startDate)){
-            throw new RuntimeException("La date de fin doit etre apres la date de debut");
+        if(this.startDate != null && endDate != null && endDate.isBefore(startDate)) {
+            throw new RuntimeException("La date de fin doit etre après la date de debut");
         }
+
         this.endDate = endDate;
     }
 
     public void setNbStudent(byte nbStudent) {
-        if (nbStudent <= 0){
-            throw new RuntimeException("Le nombre de stagiere dois etre positif");
+        if(nbStudent <= 0) {
+            throw new RuntimeException("Le nombre de stagiaire doit être positif");
         }
+
         this.nbStudent = nbStudent;
     }
 }
